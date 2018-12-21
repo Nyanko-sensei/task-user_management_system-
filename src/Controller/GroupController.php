a8819125fd0a2bd6ace88d7c059cee33d2eabbd6
Gediminas <radzeviciusgediminas@gmail.com>
2018-12-21 02:18:05 +0200

    method to set relationship between users and userGroups

--- src/Controller/GroupController.php
+++ src/Controller/GroupController.php
@@ -4,24 +4,30 @@ namespace App\Controller;
 
 use App\Entity\UserGroup;
 use App\Repository\UserGroupRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Routing\ClassResourceInterface;
 use FOS\RestBundle\View\View;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 class GroupController extends FOSRestController implements ClassResourceInterface
 {
     private $entityManager;
     private $userGroupRepository;
+    private $userRepository;
 
     public function __construct(
         EntityManagerInterface $entityManager,
-        UserGroupRepository $userGroupRepository
+        UserGroupRepository $userGroupRepository,
+        UserRepository $userRepository
     ) {
         $this->entityManager       = $entityManager;
         $this->userGroupRepository = $userGroupRepository;
+        $this->userRepository      = $userRepository;
     }
 
     public function cgetAction()
@@ -68,7 +74,7 @@ class GroupController extends FOSRestController implements ClassResourceInterfac
         $name = $request->get('name');
         $userGroup = $this->userGroupRepository->findOneBy(['name' => $name]);
         if ($userGroup) {
-            throw new HttpException(422, "User group with this name already exists");
+            throw new HttpException(409, "User group with this name already exists");
         }
 
         $userGroup = new UserGroup();
@@ -84,13 +90,60 @@ class GroupController extends FOSRestController implements ClassResourceInterfac
     public function deleteAction(int $userGroupId)
     {
         $userGroup = $this->userGroupRepository->find($userGroupId);
+
         if ($userGroup) {
+            if ($userGroup->getUsers()->count() > 0) {
+                throw new HttpException(409, "Can't delete groupe with users");
+            }
+
             $this->entityManager->remove($userGroup);
             $this->entityManager->flush();
         }
 
         $view = new View([]);
         $view->setFormat('json');
+        return $this->handleView($view);
+    }
+
+
+    public function linkUserGroupUserAction(int $groupId, int $userId)
+    {
+        $user = $this->userRepository->find($userId);
+        $userGroup = $this->userGroupRepository->find($groupId);
+
+        if (!$user || !$userGroup) {
+            throw new HttpException(404, "User or group not found");
+        }
+
+        $userGroup->addUser($user);
+        $this->entityManager->persist($userGroup);
+        $this->entityManager->flush();
+
+        $serializer = new Serializer(array(new ObjectNormalizer()));
+        $data = $serializer->normalize($userGroup , null, array('attributes' => array('name', 'Users' => ['username'])));
+
+        $view = $this->view($data, 200);
+        return $this->handleView($view);
+    }
+
+    public function unlinkUserGroupUserAction(int $groupId, int $userId)
+    {
+
+        $user = $this->userRepository->find($userId);
+        $userGroup = $this->userGroupRepository->find($groupId);
+
+        if (!$user || !$userGroup) {
+            throw new HttpException(404, "User or group not found");
+        }
+
+        $userGroup->removeUser($user);
+        $this->entityManager->persist($userGroup);
+        $this->entityManager->flush();
+
+        $serializer = new Serializer(array(new ObjectNormalizer()));
+        $data = $serializer->normalize($userGroup , null, array('attributes' => array('name', 'Users' => ['username'])));
+
+        $view = $this->view($data, 200);
         return $this->handleView($view);
     }
 
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Repository\UserGroupRepository;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\FOSRestController;
@@ -10,26 +11,33 @@ use FOS\RestBundle\Routing\ClassResourceInterface;
 use FOS\RestBundle\View\View;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 class UserController extends FOSRestController implements ClassResourceInterface
 {
     private $entityManager;
     private $userRepository;
+    private $userGroupRepository;
 
     public function __construct(
         EntityManagerInterface $entityManager,
-        UserRepository $userRepository
+        UserRepository $userRepository,
+        UserGroupRepository $userGroupRepository
     ) {
         $this->entityManager       = $entityManager;
         $this->userRepository      = $userRepository;
+        $this->userGroupRepository      = $userGroupRepository;
     }
 
     public function cgetAction()
     {
-        $entities = $this->userRepository->findAll();
+        $users = $this->userRepository->findAll();
 
-        $view = new View($entities);
-        $view->setFormat('json');
+        $serializer = new Serializer(array(new ObjectNormalizer()));
+        $data = $serializer->normalize($users, null, array('attributes' => array('username', 'UserGroups' => ['name'])));
+
+        $view = $this->view($data, 200);
         return $this->handleView($view);
     }
 
@@ -41,8 +49,10 @@ class UserController extends FOSRestController implements ClassResourceInterface
             throw new HttpException(404, "User not found");
         }
 
-        $view = new View($user);
-        $view->setFormat('json');
+        $serializer = new Serializer(array(new ObjectNormalizer()));
+        $data = $serializer->normalize($user, null, array('attributes' => array('username', 'UserGroups' => ['name'])));
+
+        $view = $this->view($data, 200);
         return $this->handleView($view);
     }
 
@@ -54,21 +64,23 @@ class UserController extends FOSRestController implements ClassResourceInterface
             throw new HttpException(404, "User not found");
         }
 
-        $user->setUsername($request->get('name'));
+        $user->setUsername($request->get('username'));
         $this->entityManager->persist($user);
         $this->entityManager->flush();
 
-        $view = new View($user);
-        $view->setFormat('json');
+        $serializer = new Serializer(array(new ObjectNormalizer()));
+        $data = $serializer->normalize($user, null, array('attributes' => array('username', 'UserGroups' => ['name'])));
+
+        $view = $this->view($data, 200);
         return $this->handleView($view);
     }
 
     public function postAction(Request $request)
     {
-        $name = $request->get('name');
+        $name = $request->get('username');
         $user = $this->userRepository->findOneBy(['username' => $name]);
         if ($user) {
-            throw new HttpException(422, "User with this name already exists");
+            throw new HttpException(409, "User with this name already exists");
         }
 
         $user = new User();
@@ -76,8 +88,10 @@ class UserController extends FOSRestController implements ClassResourceInterface
         $this->entityManager->persist($user);
         $this->entityManager->flush();
 
-        $view = new View($user);
-        $view->setFormat('json');
+        $serializer = new Serializer(array(new ObjectNormalizer()));
+        $data = $serializer->normalize($user, null, array('attributes' => array('username', 'UserGroups' => ['name'])));
+
+        $view = $this->view($data, 200);
         return $this->handleView($view);
     }
 
@@ -91,6 +105,47 @@ class UserController extends FOSRestController implements ClassResourceInterface
 
         $view = new View([]);
         $view->setFormat('json');
+        return $this->handleView($view);
+    }
+
+    public function linkUserUserGroupAction(int $userId, int $groupId)
+    {
+        $user = $this->userRepository->find($userId);
+        $userGroup = $this->userGroupRepository->find($groupId);
+
+        if (!$user || !$userGroup) {
+            throw new HttpException(404, "User or group not found");
+        }
+
+        $user->addUserGroup($userGroup);
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        $serializer = new Serializer(array(new ObjectNormalizer()));
+        $data = $serializer->normalize($user, null, array('attributes' => array('username', 'UserGroups' => ['name'])));
+
+        $view = $this->view($data, 200);
+        return $this->handleView($view);
+    }
+
+    public function unlinkUserUserGroupAction(int $userId, int $groupId)
+    {
+
+        $user = $this->userRepository->find($userId);
+        $userGroup = $this->userGroupRepository->find($groupId);
+
+        if (!$user || !$userGroup) {
+            throw new HttpException(404, "User or group not found");
+        }
+
+        $user->removeUserGroup($userGroup);
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        $serializer = new Serializer(array(new ObjectNormalizer()));
+        $data = $serializer->normalize($user, null, array('attributes' => array('username', 'UserGroups' => ['name'])));
+
+        $view = $this->view($data, 200);
         return $this->handleView($view);
     }
 }
